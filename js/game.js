5a058398a6ad19e03042cc7f1131259cfdebd3b3
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-11 10:14:17 -0300

    Criando elemento img na tela e manipulando sua posição

--- js/game.js
+++ js/game.js
@@ -6,11 +6,33 @@ function ajustaTamanhoPalcoJogo(){
 	  altura = window.innerHeight;
 	  largura = window.innerWidth;
 
-
-      console.log("Altura: "+altura);
 	  console.log("largura: "+largura);
+      console.log("Altura: "+altura);
+
 }
+
 
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+
+// Pegando os valores de altura e largura de forma randomica
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+console.log(posicaoX, posicaoY)
+
+// Criando elemento HTML
+var mosquito = document.createElement('img')
+
+mosquito.src = "img/mosca.png"
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+
+document.body.appendChild(mosquito)
+
+}
