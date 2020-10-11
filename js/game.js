83de933f71eb999c1a28adba63a4ff7f46152f63
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-11 11:47:35 -0300

    Criando lógica para perder vidas

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo(){
 	  altura = window.innerHeight;
@@ -19,6 +20,16 @@ function posicaoRandomica() {
 
 if(document.getElementById('mosquito')){
 	document.getElementById('mosquito').remove()
+	
+
+	if(vidas > 3){
+		alert("Você perdeu")
+	}
+
+	else{
+		document.getElementById('v' +vidas).src = 'img/coracao_vazio.png'
+		vidas++;
+	}
 }
 
 
@@ -40,6 +51,9 @@ mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
 mosquito.id = 'mosquito'
+mosquito.onclick = function(){
+	this.remove()
+}
 
 
 document.body.appendChild(mosquito)
