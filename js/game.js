633aa4528a5329f9c3b6b516d046a2e9033ca46f
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-11 10:17:02 -0300

    Criando lógica para evitar valores negativos de altura e largura

--- js/game.js
+++ js/game.js
@@ -21,6 +21,9 @@ function posicaoRandomica() {
 var posicaoX = Math.floor(Math.random() * largura) - 90
 var posicaoY = Math.floor(Math.random() * altura) - 90
 
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
 console.log(posicaoX, posicaoY)
 
 // Criando elemento HTML
